710a1e10c56a983bd69d6b6f7cd291ca4d5383e4
pchomesolution973-spec <pchomesolution973@gmail.com>
2025-11-19 03:53:42 -0300

    Create apps.jsx

---
+++ src/apps.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Frontend React + Tailwind OK
+      </h1>
+    </div>
+  );
+}
